84ef0eaaad07bb01a1e443918a935bcab3b73804
Alex Johnson <alextilman30@gmail.com>
2024-12-15 02:53:24 -0500

    Added a functioning map with most efficient routes and fixed the Find Route button. I also changed/ fixed the nav buttons.

--- scripts/index.js
+++ scripts/index.js
@@ -41,3 +41,22 @@ api.getResortData()
   .catch((error) => {
     console.error('Error fetching resort data:', error);
   });
+
+
+  // I was tired and didnt feel like selecting all of the <a> elements so this selects all and makes the transition smooth. --Alex
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      const targetID = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetID);
+
+      if (targetElement) {
+        const headerHeight = document.querySelector('header').offsetHeight;
+        window.scrollTo({
+          top: targetElement.offsetTop - headerHeight,
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
